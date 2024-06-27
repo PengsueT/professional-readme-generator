19cb5ad71a11bfe2f24c09b3db801665b4d35237
Charles Thao <thaocharles23@gmail.com>
2024-06-27 13:23:55 -0500

    added .gitignore and updated the questions for my generator

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide the installation instructions:',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage information:',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide contribution guidelines:',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide test instruction:',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your Github username:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
